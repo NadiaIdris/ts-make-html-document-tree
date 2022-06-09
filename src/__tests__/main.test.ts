0bd3bf9eaf52713531b009dea63e34cb3952df76
Nadia Idris <nadia@fasterlap.com>
2022-06-09 03:41:59 -0700

    Update test, refactor main, create ITag interface

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,55 +1,47 @@
 import { Tag } from '../main'
 
-test("addClass works", () => { 
-  (function() {
-    // should print '<html class="blue-theme"></html>'
-    const html = new Tag("html")
-    html.addClass("blue-theme")
+test("addClass works", () => {
+  const html = new Tag("html")
+  html.addClass("blue-theme")
 
-    const expectedResult = 
-      "\n" +
-      '<html class="blue-theme">' +
-      "\n" +
-      "</html>"
+  const expectedResult =
+    "\n" +
+    '<html class="blue-theme">' +
+    "\n" +
+    "</html>"
 
-    expect(html.printTree()).toEqual(expectedResult)
-    html.addClass("main-content")
+  expect(html.printTree()).toEqual(expectedResult)
+  html.addClass("main-content")
 
-    const expectedResultWithTwoClasses = 
+  const expectedResultWithTwoClasses =
     "\n" +
     '<html class="blue-theme main-content">' +
     "\n" +
     "</html>"
-    expect(html.printTree()).toEqual(expectedResultWithTwoClasses)
-  })();
+  expect(html.printTree()).toEqual(expectedResultWithTwoClasses)
 })
 
-test("appendChild works", () => { 
-  (function() {
-    // should print '<html class="blue-theme"><body></body></html>'
-    const html = new Tag("html")
-    html.addClass("blue-theme")
-    const body = new Tag("body")
-    const div = new Tag("div")
-    html.appendChild(body)
-    body.appendChild(div)
+test("appendChild works", () => {
+  const html = new Tag("html")
+  html.addClass("blue-theme")
+  const body = new Tag("body")
+  const div = new Tag("div")
+  html.appendChild(body)
+  body.appendChild(div)
 
-    const expectedResult = 
-      "\n" +
-      '<html class="blue-theme">' +
-      "\n" +
-      "  <body>" +
-      "\n" +
-      "    <div>" +
-      "\n" +
-      "    </div>" +
-      "\n" +
-      "  </body>" +
-      "\n" +
-      "</html>"
+  const expectedResult =
+    "\n" +
+    '<html class="blue-theme">' +
+    "\n" +
+    "  <body>" +
+    "\n" +
+    "    <div>" +
+    "\n" +
+    "    </div>" +
+    "\n" +
+    "  </body>" +
+    "\n" +
+    "</html>"
 
-    console.log(html.printTree())
-    console.log((expectedResult))
-    expect(html.printTree()).toEqual(expectedResult)
-  })();
+  expect(html.printTree()).toEqual(expectedResult)
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,49 @@
-let spaceCounter = 0;
+interface ITag {
+  name: string;
+  class: string[];
+  children: ITag[];
+  printTree: () => string;
+  addClass: (className: string) => void;
+  appendChild: (child: ITag) => void;
+}
+
+let spaceCounter = 0
 const spaceCounterIncrementer = 2;
 const whiteSpace = " ";
 
-export class Tag {
+export class Tag implements ITag {
   name: string;
   class: string[] = [];
-  children: Tag[] = [];
+  children: ITag[] = [];
 
   constructor(tagName: string) {
     this.name = tagName;
   }
+
+  printTree = (): string => {
+    const openingTag = `<${this.name}${this.populateClasses()}>`;
+    const closingTag = `</${this.name}>`;
+    const children = `${this.populateChildren()}`;
+
+    const tag =
+      "\n" +
+      whiteSpace.repeat(spaceCounter) +
+      openingTag +
+      children +
+      "\n" +
+      whiteSpace.repeat(spaceCounter) +
+      closingTag;
+
+    return tag;
+  };
+
+  addClass = (classToAdd: string): void => {
+    this.class.push(classToAdd);
+  };
+
+  appendChild = (childTag: ITag): void => {
+    this.children.push(childTag);
+  };
 
   private populateClasses = () => {
     if (this.hasClasses()) {
@@ -34,74 +68,7 @@ export class Tag {
     return childrenString;
   };
 
-  printTree = (): string => {
-    const openingTag = `<${this.name}${this.populateClasses()}>`;
-    const closingTag = `</${this.name}>`;
-    const children = `${this.populateChildren()}`;
-
-    const tag =
-      "\n" +
-      whiteSpace.repeat(spaceCounter) +
-      openingTag +
-      children +
-      "\n" +
-      whiteSpace.repeat(spaceCounter) +
-      closingTag;
-
-    return tag;
-  };
-
-  addClass = (classToAdd: string): void => {
-    this.class.push(classToAdd);
-  };
-
-  appendChild = (childTag: Tag): void => {
-    this.children.push(childTag);
-  };
-
   private hasClasses = (): boolean => {
     return this.class.length > 0;
   };
 }
-// // Case 2: append_child works.
-// (function() {
-//   // should print '<html class="blue-theme"><body><div></div></body></html>'
-//   const html = new Tag("html")
-//   html.add_class("blue-theme")
-//   const body = new Tag("body")
-//   const div = new Tag("div")
-//   html.append_child(body)
-//   body.append_child(div)
-//   expect(html.to_string()).to.equal('<html class="blue-theme"><body><div></div></body></html>')
-// })();
-
-// // Case 3: to_string works.
-// (function() {
-//   //  should print:
-//   //  <html class="blue-theme">
-//   //    <body>
-//   //      <div>
-//   //      </div>
-//   //    </body>
-//   //  </html>
-//   const html = new Tag("html")
-//   const body = new Tag("body")
-//   const div = new Tag("div")
-//   html.addClass("blue-theme")
-//   html.appendChild(body)
-//   body.appendChild(div)
-  
-//   const expectedStructure = 
-// `
-// <html class="blue-theme">
-//   <body>
-//     <div>
-//     </div>
-//   </body>
-// </html>
-// `
-
-//   console.log(html.printTree())
-//   console.log(expectedStructure)
-//   expect(html.printTree()).to.equal(expectedStructure)
-// })();
